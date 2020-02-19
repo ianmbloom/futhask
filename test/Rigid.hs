455b854d8a9f4423956d0782f01491942c2a0be1
Gusten Isfeldt <gusten.isfeldt@gmail.com>
2020-02-20 00:39:03 +0100

    error messages

--- test/Rigid.hs
+++
@@ -1,6 +0,0 @@
-
-module Rigid (module Context, module Config, module TypeClasses, module FT) where
-import Rigid.Context
-import Rigid.Config hiding (setOption)
-import Rigid.TypeClasses hiding (FutharkObject, FutharkArray)
-import Rigid.FT
